c19375eda3f242a8305885334d5b98fdd3fda310
iam-chrisjim <chrisjimphotos@gmail.com>
2017-08-05 23:56:52 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,5 +11,13 @@ $(document).ready(function(){
 $(document).on('click.bs.collapse.data-api touchstart.bs.collapse.data-api', '[data-toggle="collapse"]', function (e) { 
 });
 
+/* iphone screens media query in JavaScript */
+var mqls = [ // list of window.matchMedia() queries
+  window.matchMedia("(min-width: 300px)"),
+  window.matchMedia("(max-width: 800px)"),
+];
 
+if (mqls[0].matches && mqls[1].matches) {
+  $("div.direction-r").removeClass("direction-r").addClass("direction-l");
+}
 
